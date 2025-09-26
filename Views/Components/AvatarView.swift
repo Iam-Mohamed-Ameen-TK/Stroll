41bd070390fc7053b3cdcb802fbc4bbbffc035f3
Iam-Mohamed-Ameen-TK <mohamedameentk@gmail.com>
2025-09-26 08:59:17 +0530

    Creating after avatars and adding avatar images to the asset folder

--- Views/Components/AvatarView.swift
+++ Views/Components/AvatarView.swift
@@ -6,13 +6,41 @@
 //
 
 import SwiftUI
+import UIKit
 
 struct AvatarView: View {
+    let imageName: String?
+    let size: CGFloat
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Group {
+            if let name = imageName, let uiImage = UIImage(named: name) {
+                Image(uiImage: uiImage)
+                    .resizable()
+            } else {
+                Image(systemName: "person.crop.circle.fill")
+                    .resizable()
+                    .renderingMode(.template)
+                    .foregroundColor(.gray)
+            }
+        }
+        .scaledToFill()
+        .frame(width: size, height: size)
+        .clipShape(Circle())
+        .overlay(Circle().stroke(Color.white.opacity(0.12), lineWidth: 2))
+        .shadow(radius: 2)
     }
 }
 
-#Preview {
-    AvatarView()
+struct AvatarView_Previews: PreviewProvider {
+    static var previews: some View {
+        HStack(spacing: 20) {
+            AvatarView(imageName: nil, size: 64)              // fallback
+            AvatarView(imageName: "avatar1", size: 64)        // placeholder (falls back if missing)
+            AvatarView(imageName: "avatar2", size: 64)
+        }
+        .padding()
+        .previewLayout(.sizeThatFits)
+        .preferredColorScheme(.dark)
+    }
 }
