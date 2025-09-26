6346a2f6913e3db0468a26eaaadfd6a463660ebb
Iam-Mohamed-Ameen-TK <mohamedameentk@gmail.com>
2025-09-26 08:31:54 +0530

    Initial Commit

---
+++ Stroll/StrollApp.swift
@@ -0,0 +1,17 @@
+//
+//  StrollApp.swift
+//  Stroll
+//
+//  Created by Mohamed Ameen on 26/09/25.
+//
+
+import SwiftUI
+
+@main
+struct StrollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
